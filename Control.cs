1d0ac9749c8700e00fa1c619d4d3c2809fcf9dd5
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-06-21 19:15:46 +0900

    add: control code

--- Control.cs
+++ Control.cs
@@ -11,5 +11,22 @@ public partial class Control : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		float amount = 5;
+		if (Input.IsKeyPressed((Key)(int)Key.W))
+		{
+			Position += new Vector2(0, -amount);
+		}
+		if (Input.IsKeyPressed((Key)(int)Key.A))
+		{
+			Position += new Vector2(-amount, 0);
+		}
+		if (Input.IsKeyPressed((Key)(int)Key.S))
+		{
+			Position += new Vector2(0, amount);
+		}
+		if (Input.IsKeyPressed((Key)(int)Key.D))
+		{
+			Position += new Vector2(amount, 0);
+		}
 	}
 }
