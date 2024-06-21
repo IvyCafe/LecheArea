3ccfd87c3aa3b5c34a3ec8fbbaeb1f37d5d35ed7
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-06-21 19:24:17 +0900

    refactor: key input method

--- Control.cs
+++ Control.cs
@@ -12,19 +12,19 @@ public partial class Control : Sprite2D
 	public override void _Process(double delta)
 	{
 		float amount = 5;
-		if (Input.IsKeyPressed((Key)(int)Key.W))
+		if (Input.IsKeyPressed(Key.W))
 		{
 			Position += new Vector2(0, -amount);
 		}
-		if (Input.IsKeyPressed((Key)(int)Key.A))
+		if (Input.IsKeyPressed(Key.A))
 		{
 			Position += new Vector2(-amount, 0);
 		}
-		if (Input.IsKeyPressed((Key)(int)Key.S))
+		if (Input.IsKeyPressed(Key.S))
 		{
 			Position += new Vector2(0, amount);
 		}
-		if (Input.IsKeyPressed((Key)(int)Key.D))
+		if (Input.IsKeyPressed(Key.D))
 		{
 			Position += new Vector2(amount, 0);
 		}
